3103b0de383c3aef34ec601cf80de594b7938ace
Sumit Yadav <114563493+YuvaSky@users.noreply.github.com>
2022-10-19 17:42:12 +0530

    Add files via upload

---
+++ Arrayeven.java
@@ -0,0 +1,27 @@
+package Corejava;
+
+import java.util.Scanner;
+
+public class Arrayeven {
+public static void main(String []args){
+	Scanner sc = new Scanner(System.in);
+	System.out.println("enter the input");
+	int a = sc.nextInt();
+	// declaring the array
+	int arr[] = new int[10];
+	System.out.println("enter the element");
+	for(int i=0; i<a; i++) {
+		arr[i] = sc.nextInt();
+	}
+	// traversing the array
+	System.out.println("even numbers");
+			for(int i=0;i<a;i++) {
+				if(arr[i]%2 == 0) 
+				System.out.println(arr[i]+" ");
+			}
+             }
+			}
+				
+			
+
+
