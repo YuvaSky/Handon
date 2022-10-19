44ef15ede4d3f151b5805b40723be27a496a931f
Sumit Yadav <114563493+YuvaSky@users.noreply.github.com>
2022-10-19 17:41:49 +0530

    Add files via upload

---
+++ ArrayStringvalue.java
@@ -0,0 +1,12 @@
+package Corejava;
+
+public class ArrayStringvalue {
+	 static void main(String [] args) {
+		//Declaring and initiliztion the String
+		String a[] = {"sum","min","ani"};
+		// traverazing the array
+	   for(int i = 0; i<a.length;i++) {
+		   System.out.println(a[i]);
+	   }
+	}
+}
