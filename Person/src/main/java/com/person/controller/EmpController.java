d3770b9c4e0ae64f52150f957f569f6767a48ffc
Sumit Yadav <114563493+YuvaSky@users.noreply.github.com>
2024-05-04 12:20:37 +0530

    Update EmpController.java

--- Person/src/main/java/com/person/controller/EmpController.java
+++ Person/src/main/java/com/person/controller/EmpController.java
@@ -1,38 +1,48 @@
 package com.person.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.person.entities.Emp;
+import com.person.entities.Emp_temp;
+import com.person.helper.Helper;
 import com.person.service.EmpService;
 
 
-
+@CrossOrigin("*")
 @RestController
-public class EmpController {
+public class ExcelController {
+	
 	@Autowired
-	private EmpService empservice;
+	EmpService empService;
 	
-	@GetMapping("/emps")
-	public List<Emp> getEmp(){
-		return this.empservice.getAllEmp();
-	}
-	
-	@PostMapping("/emps")
-	public Emp addEmp(@RequestBody Emp ep) {
-		Emp e = this.empservice.addBook(ep);
-		return e;
-	}
-    @DeleteMapping("/emps/{eid}")
-    public void deleteEmp(@PathVariable("eid") int eid) {
-		this. empservice.deleteEmp(eid);
-    }
-    
+	 @PostMapping("/upload")
+	  public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file) {	   
+
+	    if (Helper.checkExcelFormat(file)) {
+	      this.empService.save(file);
+	      return ResponseEntity.ok(Map.of("message", getAllEmptemp()));
+	        
+	    }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("please upload only");
+	     }
+	 @GetMapping("/")
+	 public List<Emp> getAllEmp(){
+		 return this.empService.getAllEmployees();
+	 }
+	 @GetMapping("/emptemp")
+	 public List<Emp_temp> getAllEmptemp(){
+		 return this.empService.getAllEmptemp();
+	 }
 }
+	
