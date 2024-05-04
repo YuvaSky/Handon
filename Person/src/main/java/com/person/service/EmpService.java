3584a3be8d77aa14f0b4ca05b09f3260b234065e
Sumit Yadav <114563493+YuvaSky@users.noreply.github.com>
2024-05-04 12:21:16 +0530

    Update EmpService.java

--- Person/src/main/java/com/person/service/EmpService.java
+++ Person/src/main/java/com/person/service/EmpService.java
@@ -1,37 +1,76 @@
 package com.person.service;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.person.Dao.EmpDao;
+import com.person.Dao.Emp_tempDao;
 import com.person.entities.Emp;
+import com.person.entities.Emp_temp;
+import com.person.helper.Helper;
 
-
-@Component
+@Service
 public class EmpService {
-	
-	@Autowired
-	private EmpDao empdao;
-	
-	public List<Emp> getAllEmp(){
-		List<Emp> list =(List<Emp>)this.empdao.findAll() ;
-		return list;
-	}
-	public Emp getEmpById(int id) {
-		Emp emp =null;
-		//book = list.stream().filter(e->e.getId()==id).findFirst().get();
-		emp =this.empdao.findById(id);	
-		return emp;
-	}
-	// add Emp
-	public Emp addBook(Emp e) {
-		Emp result =this.empdao.save(e);
-		return result;		
-	}
-	public void deleteEmp(int eid) {
-		empdao.deleteById(eid);
-	}
 
+    @Autowired
+    private EmpDao empDao;
+
+    @Autowired
+    private Emp_tempDao empTempDao;
+
+ 
+    public void save(MultipartFile file) {
+        try {
+            List<Emp> employees = Helper.excelToEmps(file.getInputStream());
+            for (Emp employee : employees) {
+                if (empDao.existsByNameAndRole(employee.getName(), employee.getRole())) {
+                    // If duplicate data found, save to temporary table
+            
+                    saveToTemporaryTable(employee,"failure","this data is duplicate");
+                } else {
+                    // If no duplicate is found, save the employee object to the main table
+                    empDao.save(employee);
+                    saveToTemporaryTable(employee, "success","this data is not duplicate");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    /*public void printEmpTempData() {
+        List<Emp_temp> empTempList = empTempDao.findAll();
+        System.out.println("Emp_temp Data:");
+        for (Emp_temp empTemp : empTempList) {
+            System.out.println("Name: " + empTemp.getName() + ", Role: " + empTemp.getRole() + ", Status: " + empTemp.isStatus() + ", Message: " + empTemp.getMeassage());
+        }
+    }*/
+
+
+
+	private void saveToTemporaryTable(Emp employee, String status,String meassage) {
+        Emp_temp empTemp = new Emp_temp();
+        empTemp.setName(employee.getName());
+        empTemp.setRole(employee.getRole());
+        empTemp.setStatus(status);
+        if (status.equals("failure")) {
+            empTemp.setMeassage("this data is duplicate");
+        } else {
+            empTemp.setMeassage("data saved successfully");
+        }
+        empTemp.setMeassage(meassage);
+        empTempDao.save(empTemp);
+    }
+
+    public List<Emp> getAllEmployees() {
+         List<Emp> all = empDao.findAll();         
+		return all;        
+    }
+    public List<Emp_temp> getAllEmptemp() {
+        List<Emp_temp> all = empTempDao.findAll();         
+		return all;        
+   }
 }
